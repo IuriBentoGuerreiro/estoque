61f495d8974cac8c4f1aaf66c8b73a157d082923
iuri <bentoiuri@gmail.com>
2024-04-25 08:57:44 -0300

    implementa itemVendaRepository

---
+++ src/main/java/com/iuri/mercadinho/repository/ItemVendaRepository.java
@@ -0,0 +1,7 @@
+package com.iuri.mercadinho.repository;
+
+import com.iuri.mercadinho.model.ItemVenda;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemVendaRepository extends JpaRepository<ItemVenda, Integer> {
+}
